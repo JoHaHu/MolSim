41a0e7badb1962269c0493cf55f4f47ba85b7ccb
Johannes Hupe <mail@johupe.com>
2024-05-02 22:47:30 +0200

    feat: added new Particle Container

--- src/ParticleContainer.cpp
+++ src/ParticleContainer.cpp
@@ -1,30 +1,51 @@
 #include "ParticleContainer.h"
 #include <iostream>
 
-void ParticleContainer::addParticle(const Particle& particle) {
-    particles.push_back(particle);
+std::pair<Particle &, Particle &> ParticleContainer::Iterator::operator*() {
+  return {*i1, *i2};
 }
 
-void ParticleContainer::printPairs() const {
-    for (auto i = particles.begin(); i != particles.end(); ++i) {
-        for (auto j = std::next(i); j != particles.end(); ++j) {
-            std::cout << "Particle Pair: (" << i->toString() << ") and (" << j->toString() << ")\n";
-        }
-    }
+ParticleContainer::Iterator &ParticleContainer::Iterator::operator++() {
+
+  if (++i2 <= end && i1 <= end) {
+    return *this;
+  } else if (++i1 <= end) {
+    i2 = std::next(i1);
+    return *this;
+  } else {
+    i1 = end;
+    i2 = end;
+    return *this;
+  }
+}
+
+ParticleContainer::Iterator ParticleContainer::Iterator::operator++(int) {
+  Iterator tmp = *this;
+  ++(*this);
+  return tmp;
+}
+
+bool ParticleContainer::Iterator::operator==(Iterator &i) {
+  return i1 == i.i1 && i2 == i.i2 && end == i.end;
+}
+ParticleContainer::Iterator::Iterator(std::vector<Particle>::iterator i1,
+                                      std::vector<Particle>::iterator i2,
+                                      std::vector<Particle>::iterator end) : i1(i1), i2(i2), end(end) {
+
 }
 
 ParticleContainer::Iterator ParticleContainer::begin() {
-    return particles.begin();
+  return {
+      particles.begin(), std::next(particles.begin()), particles.end()
+  };
 }
 
 ParticleContainer::Iterator ParticleContainer::end() {
-    return particles.end();
+  return {
+      particles.end(), particles.end(), particles.end()
+  };
 }
 
-ParticleContainer::ConstIterator ParticleContainer::begin() const {
-    return particles.begin();
-}
-
-ParticleContainer::ConstIterator ParticleContainer::end() const {
-    return particles.end();
+ParticleContainer::ParticleContainer(int capacity) {
+  particles = std::vector<Particle>(capacity);
 }
