63a782adae1478d781cde6f93aa1cc092f298072
Johannes Hupe <mail@johupe.com>
2024-05-10 13:37:41 +0200

    fix: resolved comments

--- src/ParticleContainer.cpp
+++ src/ParticleContainer.cpp
@@ -7,7 +7,7 @@
  * @return A pair consisting of references to two adjacent particles.
  */
 std::pair<Particle &, Particle &> ParticleContainer::PairIterator::operator*() {
-    return {*i1, *i2};
+  return {*i1, *i2};
 }
 
 /**
@@ -15,13 +15,13 @@ std::pair<Particle &, Particle &> ParticleContainer::PairIterator::operator*() {
  * @return Reference to the updated iterator.
  */
 ParticleContainer::PairIterator &ParticleContainer::PairIterator::operator++() {
-    if (i2 != end) ++i2;
-    if (i2 == end && i1 != end) {
-        ++i1;
-        if (i1 != end) i2 = std::next(i1);
-    }
-    if (i1 == end) i2 = end;
-    return *this;
+  if (i2 != end) ++i2;
+  if (i2 == end && i1 != end) {
+    ++i1;
+    if (i1 != end) i2 = std::next(i1);
+  }
+  if (i1 == end) i2 = end;
+  return *this;
 }
 
 /**
@@ -29,16 +29,16 @@ ParticleContainer::PairIterator &ParticleContainer::PairIterator::operator++() {
  * @return The iterator before it was incremented.
  */
 ParticleContainer::PairIterator ParticleContainer::PairIterator::operator++(int) {
-    PairIterator tmp = *this;
-    ++(*this);
-    return tmp;
+  PairIterator tmp = *this;
+  ++(*this);
+  return tmp;
 }
 
 /**
  * Checks if this iterator is equal to another iterator.
  */
 bool ParticleContainer::PairIterator::operator==(PairIterator &i) {
-    return i1 == i.i1 && i2 == i.i2 && end == i.end;
+  return i1 == i.i1 && i2 == i.i2 && end == i.end;
 }
 
 ParticleContainer::PairIterator::PairIterator(std::vector<Particle>::iterator i1,
@@ -47,32 +47,32 @@ ParticleContainer::PairIterator::PairIterator(std::vector<Particle>::iterator i1
 }
 
 ParticleContainer::PairIterator ParticleContainer::begin_pair() {
-    return {
-        particles.begin(), std::next(particles.begin()), particles.end()
-    };
+  return {
+    particles.begin(), std::next(particles.begin()), particles.end()
+  };
 }
 
 ParticleContainer::PairIterator ParticleContainer::end_pair() {
-    return {
-        particles.end(), particles.end(), particles.end()
-    };
+  return {
+    particles.end(), particles.end(), particles.end()
+  };
 }
 
 ParticleContainer::ParticleContainer(int capacity) {
-    particles = std::vector<Particle>(capacity);
+  particles = std::vector<Particle>(capacity);
 }
 
 ParticleContainer::Iterator ParticleContainer::begin() {
-    return particles.begin();
+  return particles.begin();
 }
 
 ParticleContainer::Iterator ParticleContainer::end() {
-    return particles.end();
+  return particles.end();
 }
 
 unsigned long ParticleContainer::size() {
-    return particles.size();
+  return particles.size();
 }
 
-ParticleContainer::ParticleContainer(std::vector<Particle> particles) : particles(std::move(particles)) {
+ParticleContainer::ParticleContainer(std::vector<Particle> &particles) : particles(std::move(particles)) {
 }
