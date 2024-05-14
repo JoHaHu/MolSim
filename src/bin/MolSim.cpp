787c1b0fb51017b7e4a3547d2ca2449019993521
Johannes Hupe <mail@johupe.com>
2024-05-14 22:49:46 +0200

    fmt: reformatted

--- src/bin/MolSim.cpp
+++ src/bin/MolSim.cpp
@@ -4,16 +4,16 @@
 #include <filesystem>
 #include <getopt.h>
 #include <iostream>
-#include <vector>
-#include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+#include <vector>
 
 #include "lib/simulator/Simulator.h"
 #include "lib/simulator/io/VTKPlotter.h"
 #include "lib/simulator/physics/Gravity.h"
 
 void print_usage() {
-    std::cout << "Usage: ./MolSim -f filename -e end_time -d delta_t -l log_level (trace, debug, info, warn, error, critical)" << std::endl;
+  std::cout << "Usage: ./MolSim -f filename -e end_time -d delta_t -l log_level (trace, debug, info, warn, error, critical)" << std::endl;
 }
 
 /**
@@ -27,39 +27,39 @@ void print_usage() {
  * @param log_level Stores the log level.
  * @return True if parsing succeeds and it's okay to proceed, False if help is displayed or parsing fails.
  */
-bool InitializeOptions(int argc, char *argv[], char* &filename, double &end_time, double &delta_t, std::string &log_level) {
-    int opt;
-    while ((opt = getopt(argc, argv, "hf:e:d:l:")) != -1) {
-        switch (opt) {
-            case 'h':
-                print_usage();
-                return false;
-            case 'f':
-                filename = optarg;
-                break;
-            case 'e':
-                end_time = std::stod(optarg);
-                break;
-            case 'd':
-                delta_t = std::stod(optarg);
-                break;
-            case 'l':
-                log_level = std::string(optarg);
-                break;
-            default:
-                print_usage();
-                return false;
-        }
-    }
-
-    // Check input validity
-    if (!filename || end_time == 0 || delta_t == 0) {
-        spdlog::error(
-            "Erroneous program call!\nUsage: ./MolSim -f filename -e end_time -d delta_t -l log_level (trace, debug, info, warn, error, critical)");
+bool InitializeOptions(int argc, char *argv[], char *&filename, double &end_time, double &delta_t, std::string &log_level) {
+  int opt;
+  while ((opt = getopt(argc, argv, "hf:e:d:l:")) != -1) {
+    switch (opt) {
+      case 'h':
+        print_usage();
+        return false;
+      case 'f':
+        filename = optarg;
+        break;
+      case 'e':
+        end_time = std::stod(optarg);
+        break;
+      case 'd':
+        delta_t = std::stod(optarg);
+        break;
+      case 'l':
+        log_level = std::string(optarg);
+        break;
+      default:
+        print_usage();
         return false;
     }
+  }
 
-    return true;
+  // Check input validity
+  if (!filename || end_time == 0 || delta_t == 0) {
+    spdlog::error(
+        "Erroneous program call!\nUsage: ./MolSim -f filename -e end_time -d delta_t -l log_level (trace, debug, info, warn, error, critical)");
+    return false;
+  }
+
+  return true;
 }
 
 /**
@@ -68,8 +68,8 @@ bool InitializeOptions(int argc, char *argv[], char* &filename, double &end_time
  * @param level Log level as a string.
  */
 void SetupLogger(const std::string &level) {
-    spdlog::level::level_enum log_level = spdlog::level::from_str(level);
-    LoggerManager::setupLogger(log_level);
+  spdlog::level::level_enum log_level = spdlog::level::from_str(level);
+  LoggerManager::setupLogger(log_level);
 }
 
 /**
@@ -82,42 +82,42 @@ void SetupLogger(const std::string &level) {
  * Orchestrates initialization, input validation, and simulation. Logs final output or errors.
  */
 int main(int argc, char *argv[]) {
-    char* filename = nullptr;
-    double end_time = 0.0;
-    double delta_t = 0.0;
-    std::string log_level = "info";
+  char *filename = nullptr;
+  double end_time = 0.0;
+  double delta_t = 0.0;
+  std::string log_level = "info";
 
-    if (!InitializeOptions(argc, argv, filename, end_time, delta_t, log_level)) {
-        return 1;
-    }
+  if (!InitializeOptions(argc, argv, filename, end_time, delta_t, log_level)) {
+    return 1;
+  }
 
-    SetupLogger(log_level);
-    spdlog::info("Hello from MolSim for PSE!");
+  SetupLogger(log_level);
+  spdlog::info("Hello from MolSim for PSE!");
 
-    spdlog::debug("Filename: {}", filename);
-    spdlog::debug("End Time: {}", end_time);
-    spdlog::debug("Delta T: {}", delta_t);
-    spdlog::debug("Log Level: {}", log_level);
+  spdlog::debug("Filename: {}", filename);
+  spdlog::debug("End Time: {}", end_time);
+  spdlog::debug("Delta T: {}", delta_t);
+  spdlog::debug("Log Level: {}", log_level);
 
-    std::vector<Particle> particles;
-    FileReader fileReader;
-    try {
-        spdlog::info("Reading file: {}", filename);
-        fileReader.readFile(particles, filename);
-    } catch (const std::exception &e) {
-        spdlog::error("Failed to read file: {}", e.what());
-        return 1;
-    }
+  std::vector<Particle> particles;
+  FileReader fileReader;
+  try {
+    spdlog::info("Reading file: {}", filename);
+    fileReader.readFile(particles, filename);
+  } catch (const std::exception &e) {
+    spdlog::error("Failed to read file: {}", e.what());
+    return 1;
+  }
 
-    auto plotter = VTKPlotter();
-    auto physics = Gravity();
+  auto plotter = VTKPlotter();
+  auto physics = Gravity();
 
-    auto p = std::vector<Particle>();
-    fileReader.readFile(p, filename);
-    auto particleContainer = ParticleContainer(p);
+  auto p = std::vector<Particle>();
+  fileReader.readFile(p, filename);
+  auto particleContainer = ParticleContainer(p);
 
-    auto simulator = Simulator(particleContainer, plotter, physics, 0, end_time, delta_t);
-    simulator.run();
+  auto simulator = Simulator(particleContainer, plotter, physics, 0, end_time, delta_t);
+  simulator.run();
 
-    return 0;
+  return 0;
 }
