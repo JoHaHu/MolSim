1a2a4b3e178138677e987f773198cab25421955c
Johannes Hupe <mail@johupe.com>
2024-05-14 21:12:06 +0200

    feat: added submodules

--- src/lib/utils/LoggerManager.cpp
+++ src/lib/utils/LoggerManager.cpp
@@ -2,10 +2,10 @@
 
 #include <iostream>
 
-#include "spdlog/spdlog.h"
 #include "spdlog/cfg/env.h"
 #include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/spdlog.h"
 
 /**
  * Initializes the default logger with console and file sinks.
@@ -16,21 +16,21 @@
  * @param level The log level to set for the logger.
  */
 void LoggerManager::setupLogger(spdlog::level::level_enum level) {
-    try {
-        // Create logger
-        auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/log.txt", true);
+  try {
+    // Create logger
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/log.txt", true);
 
-        std::vector<spdlog::sink_ptr> sinks {console_sink, file_sink};
-        auto logger = std::make_shared<spdlog::logger>("multi_sink", sinks.begin(), sinks.end());
+    std::vector<spdlog::sink_ptr> sinks{console_sink, file_sink};
+    auto logger = std::make_shared<spdlog::logger>("multi_sink", sinks.begin(), sinks.end());
 
-        spdlog::set_default_logger(logger);
-        spdlog::cfg::load_env_levels();
-        spdlog::set_level(level);
-        spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%^%l%$] %v");
+    spdlog::set_default_logger(logger);
+    spdlog::cfg::load_env_levels();
+    spdlog::set_level(level);
+    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S] [%^%l%$] %v");
 
-        spdlog::info("Logger initialized with level: {}", spdlog::level::to_string_view(level));
-    } catch (const spdlog::spdlog_ex &ex) {
-        std::cerr << "Log initialization failed: " << ex.what() << std::endl;
-    }
+    spdlog::info("Logger initialized with level: {}", spdlog::level::to_string_view(level));
+  } catch (const spdlog::spdlog_ex &ex) {
+    std::cerr << "Log initialization failed: " << ex.what() << std::endl;
+  }
 }
