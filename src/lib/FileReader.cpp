460cc9efebcaf53bb7066881ec15800001f6e507
Johannes Hupe <mail@johupe.com>
2024-05-14 21:12:06 +0200

    feat: added submodules

--- src/lib/FileReader.cpp
+++ src/lib/FileReader.cpp
@@ -27,45 +27,45 @@ void FileReader::readFile(std::vector<Particle> &particles, char *filename) {
   double m = NAN;
   int num_particles = 0;
 
-    std::ifstream input_file(filename);
-    std::string tmp_string;
+  std::ifstream input_file(filename);
+  std::string tmp_string;
 
-    if (input_file.is_open()) {
-        getline(input_file, tmp_string);
-        spdlog::trace("Read line: {}", tmp_string);
+  if (input_file.is_open()) {
+    getline(input_file, tmp_string);
+    spdlog::trace("Read line: {}", tmp_string);
 
-        while (tmp_string.empty() or tmp_string[0] == '#') {
-            getline(input_file, tmp_string);
-            spdlog::trace("Read line: {}", tmp_string);
-        }
-
-        std::istringstream numstream(tmp_string);
-        numstream >> num_particles;
-        spdlog::debug("Reading {} particles.", num_particles);
-        getline(input_file, tmp_string);
-        spdlog::trace("Read line: {}", tmp_string);
-
-        for (int i = 0; i < num_particles; i++) {
-            std::istringstream datastream(tmp_string);
-
-            for (auto &xj: x) {
-                datastream >> xj;
-            }
-            for (auto &vj: v) {
-                datastream >> vj;
-            }
-            if (datastream.eof()) {
-                spdlog::error("Error reading file: EOF reached unexpectedly reading from line {}", i);
-                exit(-1);
-            }
-            datastream >> m;
-            particles.emplace_back(x, v, m);
-
-            getline(input_file, tmp_string);
-            spdlog::trace("Read line: {}", tmp_string);
-        }
-    } else {
-        spdlog::error("Error: could not open file {}", filename);
-        exit(-1);
+    while (tmp_string.empty() or tmp_string[0] == '#') {
+      getline(input_file, tmp_string);
+      spdlog::trace("Read line: {}", tmp_string);
     }
+
+    std::istringstream numstream(tmp_string);
+    numstream >> num_particles;
+    spdlog::debug("Reading {} particles.", num_particles);
+    getline(input_file, tmp_string);
+    spdlog::trace("Read line: {}", tmp_string);
+
+    for (int i = 0; i < num_particles; i++) {
+      std::istringstream datastream(tmp_string);
+
+      for (auto &xj : x) {
+        datastream >> xj;
+      }
+      for (auto &vj : v) {
+        datastream >> vj;
+      }
+      if (datastream.eof()) {
+        spdlog::error("Error reading file: EOF reached unexpectedly reading from line {}", i);
+        exit(-1);
+      }
+      datastream >> m;
+      particles.emplace_back(x, v, m);
+
+      getline(input_file, tmp_string);
+      spdlog::trace("Read line: {}", tmp_string);
+    }
+  } else {
+    spdlog::error("Error: could not open file {}", filename);
+    exit(-1);
+  }
 }
--- src/lib/utils/LoggerManager.h
+++ src/lib/utils/LoggerManager.h
@@ -3,6 +3,6 @@
 #include <spdlog/sinks/basic_file_sink.h>
 
 class LoggerManager {
-public:
-    static void setupLogger(spdlog::level::level_enum level);
+ public:
+  static void setupLogger(spdlog::level::level_enum level);
 };
