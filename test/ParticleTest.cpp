f473dc50145f808cce4c05349204d714686abb4b
Johannes Hupe <mail@johupe.com>
2024-05-14 17:33:49 +0200

    fix added test

--- test/ParticleTest.cpp
+++ test/ParticleTest.cpp
@@ -1,11 +1,15 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cppcoreguidelines-avoid-magic-numbers"
+
+#include "lib/Particle.h"
+#include "lib/ParticleContainer.h"
 #include <gtest/gtest.h>
-#include "../src/ParticleContainer.h"
 
 class ParticleTest : public ::testing::Test {
- protected:
+ private:
   ParticleContainer container;
 
-  // custom ParticleContainer constructor with capacity 10 because there is no default otherwise
+ public:
   ParticleTest() : container(10) {}
 
   void SetUp() override {
@@ -13,26 +17,24 @@ class ParticleTest : public ::testing::Test {
     std::vector<Particle> particles;
 
     // Particle A
-    std::array<double, 3> coordinatesA {0.0, 0.0, 0.0};
-    std::array<double, 3> velocityA {2.0, 3.0, 3.0};
+    std::array<double, 3> coordinatesA{0.0, 0.0, 0.0};
+    std::array<double, 3> velocityA{2.0, 3.0, 3.0};
 
     // Particle B
-    std::array<double, 3> coordinatesB {1.0, 1.0, 2.0};
-    std::array<double, 3> velocityB {3.0, 2.0, 3.0};
+    std::array<double, 3> coordinatesB{1.0, 1.0, 2.0};
+    std::array<double, 3> velocityB{3.0, 2.0, 3.0};
 
     // Particle C
-    std::array<double, 3> coordinatesC {2.0, 3.0, 4.0};
-    std::array<double, 3> velocityC {5.0, 6.0, 3.0};
+    std::array<double, 3> coordinatesC{2.0, 3.0, 4.0};
+    std::array<double, 3> velocityC{5.0, 6.0, 3.0};
 
     particles.emplace_back(coordinatesA, velocityA, 1.0);
-    particles.emplace_back(coordinatesA, velocityA, 1.0);
-    particles.emplace_back(coordinatesA, velocityA, 1.0);
+    particles.emplace_back(coordinatesB, velocityB, 1.0);
+    particles.emplace_back(coordinatesC, velocityC, 1.0);
 
-    container = ParticleContainer(std::move(particles));
+    container = ParticleContainer(particles);
   }
 };
-
-
 
 TEST_F(ParticleTest, IncrementOperator_NotAtEnd) {
   SetUp();
@@ -49,7 +51,6 @@ TEST(SimpleTest, BasicAssertions) {
   EXPECT_EQ(1, 1);
 }
 
-
 TEST(PairIteratorTest, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
@@ -57,7 +58,8 @@ TEST(PairIteratorTest, BasicAssertions) {
   EXPECT_EQ(7 * 6, 42);
 }
 
-int main(int argc, char **argv) {
+auto main(int argc, char **argv) -> int {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+#pragma clang diagnostic pop
