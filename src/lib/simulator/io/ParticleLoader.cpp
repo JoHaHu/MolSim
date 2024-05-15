840e63dd0f701b9939e63e0e0b2cc7f197c7ece2
Johannes Hupe <mail@johupe.com>
2024-05-15 22:16:45 +0200

    feat: generate cuboids

--- src/lib/simulator/io/ParticleLoader.cpp
+++ src/lib/simulator/io/ParticleLoader.cpp
@@ -1,11 +1,13 @@
 
-
 #include <fstream>
+#include <ranges>
 #include <sstream>
 
 #include "ParticleLoader.h"
 #include "lib/Particle.h"
+#include "lib/utils/ArrayUtils.h"
 #include "lib/utils/LoggerManager.h"
+#include "lib/utils/MaxwellBoltzmannDistribution.h"
 #include "spdlog/spdlog.h"
 
 namespace simulator::io {
@@ -25,7 +27,7 @@ auto ParticleLoader::load_particles() -> std::tuple<ParticleContainer, simulator
   switch (model) {
     case physics::ForceModel::LennardJones: {
       auto cuboids = *parse_cuboids(input_buf);
-      auto p = generate_cuboids(cuboids);
+      auto p = generate_cuboids(cuboids, config->seed);
       particles = ParticleContainer(p);
       break;
     }
@@ -165,8 +167,25 @@ auto ParticleLoader::parse_cuboids(std::istreambuf_iterator<char> &buf) -> std::
   }
   return particles;
 }
-auto ParticleLoader::generate_cuboids(std::vector<cuboid_t> /*unused*/) -> std::vector<Particle> {
-  return std::vector<Particle>();
+static constexpr const double brownian_motion = 0.1;
+auto ParticleLoader::generate_cuboids(const std::vector<cuboid_t> &cuboids, auto seed) -> std::vector<Particle> {
+
+  auto particles = std::vector<Particle>();
+
+  for (auto const [index, cuboid] : std::views::enumerate(cuboids)) {
+    auto [position, velocity, dim, h, m, sigma] = cuboid;
+    for (auto x : std::views::iota(0, dim[0])) {
+      for (auto y : std::views::iota(0, dim[1])) {
+        for (auto z : std::views::iota(0, dim[2])) {
+          auto particle = Particle(position + std::array<double, 3>({h * static_cast<double>(x), h * static_cast<double>(y), h * static_cast<double>(z)}), velocity + maxwellBoltzmannDistributedVelocity(brownian_motion, 2, seed), m, static_cast<int>(index));
+          particles.emplace_back(particle);
+        }
+      }
+    }
+  }
+
+  particles.shrink_to_fit();
+  return particles;
 }
 auto ParticleLoader::recognize_end_of_line(std::istreambuf_iterator<char> &buf) -> bool {
   recognize_whitespace(buf);
