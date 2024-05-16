b7e7895d86f4d955d4ee5c752be70b3376c4a4da
DonJulio <crap.hey.12345@protonmail.com>
2024-05-16 12:38:22 +0200

    fix: LoggerManager now working properly again

--- src/lib/utils/LoggerManager.cpp
+++ src/lib/utils/LoggerManager.cpp
@@ -7,6 +7,30 @@
 #include <iostream>
 
 /**
+ * @brief Retrieves the spdlog logging level from the environment variable SPDLOG_LEVEL.
+ *
+ * Checks the SPDLOG_LEVEL environment variable and converts it to the corresponding
+ * spdlog::level::level_enum value. If the environment variable is not set or has
+ * an invalid value, it defaults to spdlog::level::info.
+ *
+ * @return spdlog::level::level_enum The logging level.
+ */
+spdlog::level::level_enum get_spdlog_level_from_env() {
+  const char* env_level = std::getenv("SPDLOG_LEVEL");
+  if (env_level) {
+    std::string level_str(env_level);
+    if (level_str == "trace") return spdlog::level::trace;
+    if (level_str == "debug") return spdlog::level::debug;
+    if (level_str == "info") return spdlog::level::info;
+    if (level_str == "warn") return spdlog::level::warn;
+    if (level_str == "error") return spdlog::level::err;
+    if (level_str == "critical") return spdlog::level::critical;
+    if (level_str == "off") return spdlog::level::off;
+  }
+  return spdlog::level::info;  // Default level if SPDLOG_LEVEL is not set or invalid
+}
+
+/**
  * Initializes the default logger with console and file sinks.
  *
  * Sets up a default logger to log to both the console and a file. Loads log
@@ -14,9 +38,11 @@
  *
  * @param level The log level to set for the logger.
  */
-void LoggerManager::setup_logger(std::shared_ptr<config::Config> config, spdlog::level::level_enum level) {
+void LoggerManager::setup_logger(std::shared_ptr<config::Config> config) {
   try {
-    spdlog::trace("Setting up logger with level: {}", spdlog::level::to_string_view(level));
+    // Read environment variable
+    spdlog::cfg::load_env_levels();
+    spdlog::level::level_enum level = get_spdlog_level_from_env();
 
     // Create console sink
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
@@ -34,8 +60,8 @@ void LoggerManager::setup_logger(std::shared_ptr<config::Config> config, spdlog:
 
     // Set default logger
     spdlog::set_default_logger(logger);
-    spdlog::cfg::load_env_levels();
     spdlog::set_level(level);
+
     // Disable log when io disabled
     if (config->io_interval == 0) {
       spdlog::set_level(spdlog::level::off);
