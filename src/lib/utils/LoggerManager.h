1eb4c0fb864f5a1b7275993a539b178f961be306
Johannes Hupe <mail@johupe.com>
2024-05-16 22:55:58 +0200

    doc: added doc comments to all headers

--- src/lib/utils/LoggerManager.h
+++ src/lib/utils/LoggerManager.h
@@ -3,8 +3,12 @@
 #include "config/config.h"
 #include <spdlog/sinks/basic_file_sink.h>
 
+/**
+ * a helper class providing a static function to setup the logger
+ * */
 class LoggerManager {
  public:
   LoggerManager() = delete;
+  // TODO remove the class and only keep a static function
   static void setup_logger(std::shared_ptr<config::Config> config);
 };
