4c1a2e4a41cc27e537e37ea10fc4e30ff4a3ade6
Johannes Hupe <mail@johupe.com>
2024-06-06 17:03:49 +0200

    feat: added more test

--- test/IndexTest.cpp
+++ test/IndexTest.cpp
@@ -1,15 +1,36 @@
-//#include "Particle.h"
-//#include "container/index.h"
+#include "container/index.h"
 #include <gtest/gtest.h>
 
 class SimpleIndexTest : public ::testing::Test {
 };
 
-TEST_F(SimpleIndexTest, index_conversions_including_boundaries) {
+/*
+ * Test that the index in row major ordering behaves okay
+ * */
+TEST_F(SimpleIndexTest, row_major_ordering_index) {
 
   auto bounds = std::array<double, 3>({3, 3, 3});
+  auto idx = container::index::row_major_index(bounds, 1);
+  EXPECT_EQ(idx.boundary, bounds);
+  auto dim = std::array<size_t, 3>({3, 3, 3});
+  EXPECT_EQ(idx.dimension, dim);
+  auto width = std::array<double, 3>({1, 1, 1});
+  EXPECT_EQ(idx.width, width);
+  auto radius = std::array<size_t, 3>({1, 1, 1});
+  EXPECT_EQ(idx.radius, radius);
 
-  //  auto idx = container::index::row_major_index(bounds, 1);
+  for (size_t x = 0; x < 3; ++x) {
+    for (size_t y = 0; y < 3; ++y) {
+      for (size_t z = 0; z < 3; ++z) {
+        auto new_index = idx.dimension_to_index({x, y, z});
+        EXPECT_LT(new_index, 27);
+      }
+    }
+  }
+
+  auto position = std::array<double, 3>({1.5, 1.5, 1.5});
+  auto cell = idx.dimension_to_index({1, 1, 1});
+  EXPECT_EQ(idx.position_to_index(position), cell);
 }
 
 class ZOrderIndexTest : public ::testing::Test {
