dd8063c86bb56cee717fcb4258516bfb98079747
Johannes Hupe <mail@johupe.com>
2024-06-20 20:50:09 +0200

    feat: added thermostat test

--- test/ThermostatTest.cpp
+++ test/ThermostatTest.cpp
@@ -6,7 +6,6 @@
 #include <gtest/gtest.h>
 #include <memory>// Required for std::construct_at
 
-/**
 class ThermostatTest : public ::testing::Test {
  protected:
   container::ParticleContainer<2> particles = container::ParticleContainer<2>(Particles<2>());
@@ -15,82 +14,88 @@ class ThermostatTest : public ::testing::Test {
 
   // Constants for test usage
   const double t_init = 10.0;
-  const double t_target = 20.0;
+  const double t_target = 200.0;
   const double delta_t = 10.0;
   const unsigned int seed = 12345;
 
   void SetUp() override {
     // Initialize particles
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < 100; ++i) {
       std::array<double, 2> coordinates = {static_cast<double>(i), static_cast<double>(i)};
-      std::array<double, 2> velocity = {static_cast<double>(i + 1), static_cast<double>(i + 1)};
-      particles.insert(Particle<2>(coordinates, velocity, 1.0, 1));
+      std::array<double, 2> velocity;
+      particles.insert(Particle<2>(coordinates, {0, 0}, 1.0, 1));
     }
   }
 
   void initializeThermostat(double t_init, std::optional<double> t_target, std::optional<double> delta_t, unsigned int seed) {
     thermostat = Thermostat<2>(t_init, t_target, delta_t, seed);
-    thermostat.initializeVelocities(particles, true, std::sqrt(t_init));
+    thermostat.initializeVelocities(particles, true, 0.0);
   }
 };
-**/
 
 TEST_F(ThermostatTest, test_initialize) {
   initializeThermostat(t_init, t_target, delta_t, seed);
   double temp = thermostat.calculateCurrentTemperature(particles);
-
-  EXPECT_EQ(temp, t_init);
+  EXPECT_NEAR(temp, t_init, 2);
 }
 
 /**
  * Test: test_heating
  *
  * Verifies that the thermostat correctly heats the particle system towards the target temperature.
-
+*/
 TEST_F(ThermostatTest, test_heating) {
 
   initializeThermostat(t_init, t_target, delta_t, seed);
   double old_temp = thermostat.calculateCurrentTemperature(particles);
-  thermostat.apply(particles);
-  double newTemperature = thermostat.calculateCurrentTemperature(particles);
-  EXPECT_GT(newTemperature, old_temp);
-  EXPECT_GT(newTemperature, 10);
+  for (int i = 0; i < 4; ++i) {
+    thermostat.apply(particles);
+    double newTemperature = thermostat.calculateCurrentTemperature(particles);
+    EXPECT_GT(newTemperature, old_temp);
+    EXPECT_GT(newTemperature, 10);
+    old_temp = newTemperature;
+  }
 }
- */
 
 /**
  * Test: test_cooling
  *
  * Verifies that the thermostat correctly cools the particle system towards the target temperature.
-
+*/
 TEST_F(ThermostatTest, test_cooling) {
   initializeThermostat(t_target, t_init, delta_t, seed);
   double old_temp = thermostat.calculateCurrentTemperature(particles);
-  thermostat.apply(particles);
-  double newTemperature = thermostat.calculateCurrentTemperature(particles);
-
-  EXPECT_LT(newTemperature, old_temp);
-  EXPECT_LT(newTemperature, 20);
+  for (int i = 0; i < 4; ++i) {
+    thermostat.apply(particles);
+    double newTemperature = thermostat.calculateCurrentTemperature(particles);
+    EXPECT_LT(newTemperature, old_temp);
+    EXPECT_LT(newTemperature, 200);
+    old_temp = newTemperature;
+  }
 }
-*/
 
 /**
  * Test: test_holding_temperature
  *
  * Verifies that the thermostat holds the temperature constant when the current temperature is equal to the target temperature.
-
+*/
 TEST_F(ThermostatTest, test_holding_temperature) {
-  const double t_init = 100.0;
+  const double t_init = 200.0;
   const double t_target = 200.0;
   const double delta_t = 10.0;
   const unsigned int seed = 12345;
 
   initializeThermostat(t_init, t_target, delta_t, seed);
+  // Let temp settle
+  for (int i = 0; i < 10; ++i) {
+    thermostat.apply(particles);
+  }
   double initialTemperature = thermostat.calculateCurrentTemperature(particles);
-  thermostat.apply(particles);
-  double newTemperature = thermostat.calculateCurrentTemperature(particles);
+  EXPECT_NEAR(initialTemperature, 200, 1);
 
-  EXPECT_NEAR(initialTemperature, newTemperature, tolerance);
+  for (int i = 0; i < 4; ++i) {
+    thermostat.apply(particles);
+    double newTemperature = thermostat.calculateCurrentTemperature(particles);
+    EXPECT_NEAR(newTemperature, 200, 1);
+  }
 }
-
-*/
