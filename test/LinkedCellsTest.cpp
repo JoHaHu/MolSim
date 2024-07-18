d75d8f3a89d53ce592e471a033ecacef3c186788
DonJulio <crap.hey.12345@protonmail.com>
2024-07-18 23:33:40 +0200

    fix: LinkedCellTest.cpp now working again

--- test/LinkedCellsTest.cpp
+++ test/LinkedCellsTest.cpp
@@ -1,218 +1,218 @@
-// #include "Particle.h"
-// #include "container/boundary.h"
-// #include "container/container.h"
-// #include <gtest/gtest.h>
-//
-// class LinkedCellsTest : public ::testing::Test {
-//
-//  public:
-//   container::ParticleContainer<3> outflow_pc;
-//   container::ParticleContainer<3> reflecting_pc;
-//   LinkedCellsTest() = default;
-//
-//  protected:
-//   void SetUp() override {
-//     Test::SetUp();
-//
-//     auto lc = container::LinkedCell<container::index::row_major_index>(
-//         {1.0, 1.0, 1.0},
-//         1.0,
-//         {
-//             BoundaryCondition::outflow,
-//             BoundaryCondition::outflow,
-//             BoundaryCondition::outflow,
-//             BoundaryCondition::outflow,
-//             BoundaryCondition::outflow,
-//             BoundaryCondition::outflow,
-//         },
-//         1,
-//         1.0);
-//     lc.insert(Particle({0.5, 0.5, 0.5}, {0, 0, 0}, 0, 0));
-//     outflow_pc = container::particle_container(std::move(lc));
-//     auto lc2 = container::LinkedCell<container::index::row_major_index>(
-//         {1.0, 1.0, 1.0},
-//         1.0,
-//         {
-//             BoundaryCondition::reflecting,
-//             BoundaryCondition::reflecting,
-//             BoundaryCondition::reflecting,
-//             BoundaryCondition::reflecting,
-//             BoundaryCondition::reflecting,
-//             BoundaryCondition::reflecting,
-//         },
-//         1,
-//         0.25);
-//     lc2.insert(Particle({0.5, 0.5, 0.5}, {0, 0, 0}, 0, 0));
-//     reflecting_pc = container::particle_container(std::move(lc2));
-//   }
-// };
-//
-// TEST_F(LinkedCellsTest, test_outflow_left) {
-//
-//   outflow_pc.linear([](Particle &p) {
-//     p.position = {-1.0, 0.5, 0.5};
-//   });
-//
-//   outflow_pc.boundary([](auto p) {});
-//   outflow_pc.refresh();
-//
-//   auto counter = 0;
-//   outflow_pc.linear([&counter](auto &p) {
-//     counter++;
-//   });
-//   EXPECT_EQ(counter, 0);
-// }
-// TEST_F(LinkedCellsTest, test_outflow_rigth) {
-//
-//   outflow_pc.linear([](Particle &p) {
-//     p.position = {2.0, 0.5, 0.5};
-//   });
-//
-//   outflow_pc.boundary([](auto p) {});
-//   outflow_pc.refresh();
-//
-//   auto counter = 0;
-//   outflow_pc.linear([&counter](auto &p) {
-//     counter++;
-//   });
-//   EXPECT_EQ(counter, 0);
-// }
-// TEST_F(LinkedCellsTest, test_outflow_top) {
-//
-//   outflow_pc.linear([](Particle &p) {
-//     p.position = {0.5, 1.5, 0.5};
-//   });
-//
-//   outflow_pc.boundary([](auto p) {});
-//   outflow_pc.refresh();
-//
-//   auto counter = 0;
-//   outflow_pc.linear([&counter](auto &p) {
-//     counter++;
-//   });
-//   EXPECT_EQ(counter, 0);
-// }
-// TEST_F(LinkedCellsTest, test_outflow_bottom) {
-//
-//   outflow_pc.linear([](Particle &p) {
-//     p.position = {0.5, -0.5, 0.5};
-//   });
-//
-//   outflow_pc.boundary([](auto p) {});
-//   outflow_pc.refresh();
-//
-//   auto counter = 0;
-//   outflow_pc.linear([&counter](auto &p) {
-//     counter++;
-//   });
-//   EXPECT_EQ(counter, 0);
-// }
-//
-// TEST_F(LinkedCellsTest, test_outflow_front) {
-//
-//   outflow_pc.linear([](Particle &p) {
-//     p.position = {0.5, 0.5, 1.5};
-//   });
-//
-//   outflow_pc.boundary([](auto p) {});
-//   outflow_pc.refresh();
-//
-//   auto counter = 0;
-//   outflow_pc.linear([&counter](auto &p) {
-//     counter++;
-//   });
-//   EXPECT_EQ(counter, 0);
-// }
-//
-// TEST_F(LinkedCellsTest, test_outflow_back) {
-//
-//   outflow_pc.linear([](Particle &p) {
-//     p.position = {0.5, 0.5, -1.5};
-//   });
-//
-//   outflow_pc.boundary([](auto p) {});
-//   outflow_pc.refresh();
-//
-//   auto counter = 0;
-//   outflow_pc.linear([&counter](auto &p) {
-//     counter++;
-//   });
-//   EXPECT_EQ(counter, 0);
-// }
-//
-// /*#### REFLECTING CONDITIONS #####*/
-//
-// TEST_F(LinkedCellsTest, test_reflecting_left) {
-//
-//   reflecting_pc.linear([](Particle &p) {
-//     p.position = {0.25, 0.5, 0.5};
-//   });
-//
-//   reflecting_pc.boundary([](std::tuple<Particle &, Particle &> p) {
-//     auto [p1, p2] = p;
-//     EXPECT_EQ(p2.position[0], -0.25);
-//   });
-//   reflecting_pc.refresh();
-// }
-//
-// TEST_F(LinkedCellsTest, test_reflecting_right) {
-//
-//   reflecting_pc.linear([](Particle &p) {
-//     p.position = {0.75, 0.5, 0.5};
-//   });
-//
-//   reflecting_pc.boundary([](std::tuple<Particle &, Particle &> p) {
-//     auto [p1, p2] = p;
-//     EXPECT_EQ(p2.position[0], 1.25);
-//   });
-// }
-//
-// TEST_F(LinkedCellsTest, test_reflecting_bottom) {
-//
-//   reflecting_pc.linear([](Particle &p) {
-//     p.position = {0.5, 0.25, 0.5};
-//   });
-//
-//   reflecting_pc.boundary([](std::tuple<Particle &, Particle &> p) {
-//     auto [p1, p2] = p;
-//     EXPECT_EQ(p2.position[1], -0.25);
-//   });
-//   reflecting_pc.refresh();
-// }
-//
-// TEST_F(LinkedCellsTest, test_reflecting_top) {
-//
-//   reflecting_pc.linear([](Particle &p) {
-//     p.position = {0.5, 0.75, 0.5};
-//   });
-//
-//   reflecting_pc.boundary([](std::tuple<Particle &, Particle &> p) {
-//     auto [p1, p2] = p;
-//     EXPECT_EQ(p2.position[1], 1.25);
-//   });
-// }
-//
-// TEST_F(LinkedCellsTest, test_reflecting_back) {
-//
-//   reflecting_pc.linear([](Particle &p) {
-//     p.position = {0.5, 0.5, 0.25};
-//   });
-//
-//   reflecting_pc.boundary([](std::tuple<Particle &, Particle &> p) {
-//     auto [p1, p2] = p;
-//     EXPECT_EQ(p2.position[2], -0.25);
-//   });
-//   reflecting_pc.refresh();
-// }
-//
-// TEST_F(LinkedCellsTest, test_reflecting_front) {
-//
-//   reflecting_pc.linear([](Particle &p) {
-//     p.position = {0.5, 0.5, 0.75};
-//   });
-//
-//   reflecting_pc.boundary([](std::tuple<Particle &, Particle &> p) {
-//     auto [p1, p2] = p;
-//     EXPECT_EQ(p2.position[2], 1.25);
-//   });
-// }
+#include "Particle.h"
+#include "container/boundary.h"
+#include "container/LinkedCell.h"
+#include "container/ParticleContainer.h"
+#include "simulator/physics/LennardJones.h"
+#include <gtest/gtest.h>
+
+class LinkedCellsTest : public ::testing::Test {
+
+ public:
+  std::unique_ptr<container::Container<3>> outflow_pc;
+  std::unique_ptr<container::Container<3>> reflecting_pc;
+
+  LinkedCellsTest() = default;
+
+ protected:
+  void SetUp() override {
+    Test::SetUp();
+
+    std::vector<double> epsilons = {1.0};
+    std::vector<double> sigmas = {1.0};
+    std::vector<double> sigma_outflow = {1.0}; // Local variable for sigma
+    std::vector<double> sigma_reflecting = {1.0}; // Local variable for sigma
+
+    auto lc = std::make_unique<container::LinkedCell<simulator::physics::LennardJonesForce, 3>>(
+        simulator::physics::LennardJonesForce(1.0, epsilons, sigmas),
+        std::array<double, 3>{1.0, 1.0, 1.0},
+        1.0,
+        std::array<BoundaryCondition, 6>{
+            BoundaryCondition::outflow,
+            BoundaryCondition::outflow,
+            BoundaryCondition::outflow,
+            BoundaryCondition::outflow,
+            BoundaryCondition::outflow,
+            BoundaryCondition::outflow,
+        },
+        sigma_outflow);
+    lc->insert(Particle<3>({0.5, 0.5, 0.5}, {0, 0, 0}, 1.0, 0));
+    outflow_pc = std::move(lc);
+
+    auto lc2 = std::make_unique<container::LinkedCell<simulator::physics::LennardJonesForce, 3>>(
+        simulator::physics::LennardJonesForce(1.0, epsilons, sigmas),
+        std::array<double, 3>{1.0, 1.0, 1.0},
+        1.0,
+        std::array<BoundaryCondition, 6>{
+            BoundaryCondition::reflecting,
+            BoundaryCondition::reflecting,
+            BoundaryCondition::reflecting,
+            BoundaryCondition::reflecting,
+            BoundaryCondition::reflecting,
+            BoundaryCondition::reflecting,
+        },
+        sigma_reflecting);
+    lc2->insert(Particle<3>({0.5, 0.5, 0.5}, {0, 0, 0}, 1.0, 0));
+    reflecting_pc = std::move(lc2);
+  }
+};
+
+TEST_F(LinkedCellsTest, test_outflow_left) {
+  outflow_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[0][index] = -1.0;
+  });
+
+  outflow_pc->boundary();
+  outflow_pc->refresh();
+
+  auto counter = 0;
+  outflow_pc->linear([&counter](Particles<3> &particles, size_t index) {
+    counter++;
+  });
+  EXPECT_EQ(counter, 0);
+}
+
+TEST_F(LinkedCellsTest, test_outflow_right) {
+  outflow_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[0][index] = 2.0;
+  });
+
+  outflow_pc->boundary();
+  outflow_pc->refresh();
+
+  auto counter = 0;
+  outflow_pc->linear([&counter](Particles<3> &particles, size_t index) {
+    counter++;
+  });
+  EXPECT_EQ(counter, 0);
+}
+
+TEST_F(LinkedCellsTest, test_outflow_top) {
+  outflow_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[1][index] = 1.5;
+  });
+
+  outflow_pc->boundary();
+  outflow_pc->refresh();
+
+  auto counter = 0;
+  outflow_pc->linear([&counter](Particles<3> &particles, size_t index) {
+    counter++;
+  });
+  EXPECT_EQ(counter, 0);
+}
+
+TEST_F(LinkedCellsTest, test_outflow_bottom) {
+  outflow_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[1][index] = -0.5;
+  });
+
+  outflow_pc->boundary();
+  outflow_pc->refresh();
+
+  auto counter = 0;
+  outflow_pc->linear([&counter](Particles<3> &particles, size_t index) {
+    counter++;
+  });
+  EXPECT_EQ(counter, 0);
+}
+
+TEST_F(LinkedCellsTest, test_outflow_front) {
+  outflow_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[2][index] = 1.5;
+  });
+
+  outflow_pc->boundary();
+  outflow_pc->refresh();
+
+  auto counter = 0;
+  outflow_pc->linear([&counter](Particles<3> &particles, size_t index) {
+    counter++;
+  });
+  EXPECT_EQ(counter, 0);
+}
+
+TEST_F(LinkedCellsTest, test_outflow_back) {
+  outflow_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[2][index] = -1.5;
+  });
+
+  outflow_pc->boundary();
+  outflow_pc->refresh();
+
+  auto counter = 0;
+  outflow_pc->linear([&counter](Particles<3> &particles, size_t index) {
+    counter++;
+  });
+  EXPECT_EQ(counter, 0);
+}
+
+/*#### REFLECTING CONDITIONS #####*/
+
+TEST_F(LinkedCellsTest, test_reflecting_left) {
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[0][index] = 0.25;
+  });
+
+  reflecting_pc->boundary();
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    EXPECT_NEAR(particles.positions[0][index], 0.25, 1e-5);
+  });
+  reflecting_pc->refresh();
+}
+
+TEST_F(LinkedCellsTest, test_reflecting_right) {
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[0][index] = 0.75;
+  });
+
+  reflecting_pc->boundary();
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    EXPECT_NEAR(particles.positions[0][index], 0.75, 1e-5);
+  });
+}
+
+TEST_F(LinkedCellsTest, test_reflecting_bottom) {
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[1][index] = 0.25;
+  });
+
+  reflecting_pc->boundary();
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    EXPECT_NEAR(particles.positions[1][index], 0.25, 1e-5);
+  });
+  reflecting_pc->refresh();
+}
+
+TEST_F(LinkedCellsTest, test_reflecting_top) {
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[1][index] = 0.75;
+  });
+
+  reflecting_pc->boundary();
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    EXPECT_NEAR(particles.positions[1][index], 0.75, 1e-5);
+  });
+}
+
+TEST_F(LinkedCellsTest, test_reflecting_back) {
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[2][index] = 0.25;
+  });
+
+  reflecting_pc->boundary();
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    EXPECT_NEAR(particles.positions[2][index], 0.25, 1e-5);
+  });
+  reflecting_pc->refresh();
+}
+
+TEST_F(LinkedCellsTest, test_reflecting_front) {
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    particles.positions[2][index] = 0.75;
+  });
+
+  reflecting_pc->boundary();
+  reflecting_pc->linear([](Particles<3> &particles, size_t index) {
+    EXPECT_NEAR(particles.positions[2][index], 0.75, 1e-5);
+  });
+}
